2035179ddedd2244dd12ee717528fbbf8da66e57
Sebastian Wilden <sebastian.wilden@outlook.com>
2026-03-03 20:37:20 +0100

    fix: add ssr support in login header

--- app/components/AuthControls.vue
+++ app/components/AuthControls.vue
@@ -1,19 +1,21 @@
 <template>
-  <div v-if="loggedIn" class="flex items-center gap-4">
-    {{ user?.loginname }}
-    <UButton
-      @click="
-        clear();
-        navigateTo('/login');
-      "
-      >Logout</UButton
-    >
-  </div>
-  <div v-else>
-    <UButton as-child>
-      <NuxtLink to="/login">Login</NuxtLink>
-    </UButton>
-  </div>
+  <AuthState v-slot="{ loggedIn, clear }">
+    <div v-if="loggedIn" class="flex items-center gap-4">
+      {{ user?.name }}
+      <UButton
+        @click="
+          clear();
+          navigateTo('/login');
+        "
+        >Logout</UButton
+      >
+    </div>
+    <div v-else>
+      <UButton as-child>
+        <NuxtLink to="/login">Login</NuxtLink>
+      </UButton>
+    </div>
+  </AuthState>
 </template>
 
 <script setup lang="ts">
